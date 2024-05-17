1a0266ed6f3efa84bc207bf5fa99ce644de9bba9
Diogo Castro <dcastrocs@gmail.com>
2024-05-17 16:05:03 -0300

    primeira modificação

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String cliente = ("MARIO ANDRADE");
+        String Agencia = ("067-8");
+        int numero = 1021;
+        double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por Favor, digite o número da agência: ");
+        String ag = scanner.nextLine();
+        String msg = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", cliente, ag, numero, saldo);
+        if (Objects.equals(Agencia, ag)) {
+        System.out.println(msg);
+        } else {
+            System.out.println("Agência não encontrada.");
+        }
+
     }
+    
 }
