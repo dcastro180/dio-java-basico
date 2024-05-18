b6dab834703c813f39fde5b7f1bf768ee71c7d57
Diogo Castro <dcastrocs@gmail.com>
2024-05-18 17:33:28 -0300

    atualização caixa eletronico

--- controle-de-fluxo/caixa-eletronico/src/CaixaEletronico.java
+++ controle-de-fluxo/caixa-eletronico/src/CaixaEletronico.java
@@ -1,11 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
-        
-        if (valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
-        System.out.println(saldo);
+        double valorSolicitado = 37.0;
+
+        if (valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saque realizado com sucesso! Saldo atual: " + saldo);
+            } else 
+            System.out.println("Saldo Insuficiente");
                                    
     }
 }
