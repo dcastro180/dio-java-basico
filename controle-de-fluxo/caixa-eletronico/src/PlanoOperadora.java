f71789b533cd69abd3b1834f33d0a43a9e814f77
Diogo Castro <dcastrocs@gmail.com>
2024-05-20 10:40:33 -0300

    switch case sem utilizar o break, realiza a montagem com a sequencia de melhor primeiro e basico abaixo assim ao selecionar o plano que tem o melhor aparece todos abaixo, e selecionando apenas o basico não vai aparecer as opções acima .

---
+++ controle-de-fluxo/caixa-eletronico/src/PlanoOperadora.java
@@ -0,0 +1,21 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano ="T";
+        switch (plano) {
+            case "T":{
+                System.out.println("5GB no Youtube");
+                
+            }
+            case "M":{
+                System.out.println("Whatsapp e Instagram Grátis");
+                
+            }     
+            case "B":{
+                System.out.println("100 Minutos de ligações");
+                
+            }
+            
+            
+        }
+    }
+}
