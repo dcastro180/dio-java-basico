fa28adf28c590d3bd2766f72aeeb40841f389c53
Diogo Castro <dcastrocs@gmail.com>
2024-05-20 10:23:38 -0300

    Sistema de switch case

--- controle-de-fluxo/caixa-eletronico/src/SistemaMedida.java
+++ controle-de-fluxo/caixa-eletronico/src/SistemaMedida.java
@@ -1,15 +1,31 @@
 public class SistemaMedida {
     public static void main(String[] args) {
-        String sigla = "A";
+        String sigla = "M";
 
-        if (sigla == "P")
-        System.out.println("Pequeno");
-        else if (sigla == "M")
-        System.out.println("Medio");
-        else if (sigla == "G")
-        System.out.println("Grande");
-        else
-        System.out.println("Invalido");
+        //if (sigla == "P")
+        //System.out.println("Pequeno");
+        //else if (sigla == "M")
+        //System.out.println("Medio");
+        //else if (sigla == "G")
+        //System.out.println("Grande");
+        //else
+        //System.out.println("Invalido");
+        switch (sigla) {
+            case "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case "M":{
+                System.out.println("Medio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+            default:
+            System.out.println("Invalido");
+        }
     }
     
 }
